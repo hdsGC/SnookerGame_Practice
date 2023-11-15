071d26f98b9621d004475077aea96f37820d7ea5
Leo Huang <ghuang14@u.rochester.edu>
2023-11-14 19:15:11 -0500

    Initial Assets for the Game

---
+++ Source/SnookerGame/SnookerGame.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "SnookerGame.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, SnookerGame, "SnookerGame" );
